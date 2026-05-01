ea56fe7748a5309ac11a556ab5d8011df276c8da
cai360 <archery0360@gmail.com>
2026-05-01 16:26:44 +0800

    260501 two sum

---
+++ 1-Two_sum.ts
@@ -0,0 +1,26 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+
+function twoSum(nums: number[], target: number): number[] {
+  const map =  new Map<number, number>();
+  for(let i=0; i < nums.length; i++){
+    let temp = target - nums[i]!;
+    if(map.has(temp)){
+      return [map.get(temp)!, i];
+    }
+    map.set(nums[i]!, i);
+  }
+  return [];
+}
+
+let ans = twoSum([2,7,11,15], 9);
+console.log(ans);
+
+/**
+ with hash map can reach O(n), because hash map lookup is O(1). 
+ */
+
