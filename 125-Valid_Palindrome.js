2da4d1ef800c287e9eae9eead8aceaa714643f64
cai360 <archery0360@gmail.com>
2026-05-01 17:46:19 +0800

    260501 valid paliindrome

--- 125-Valid_Palindrome.js
+++ 125-Valid_Palindrome.js
@@ -4,38 +4,33 @@ A phrase is a palindrome if, after converting all uppercase letters into lowerca
 Given a string s, return true if it is a palindrome, or false otherwise.
  */
 
-function isPalindrome(str: string): boolean{
-    if(!str) return true;
-    const theLower = str.toLowerCase();
+function isPalindrome(str){
+    const lowerStr = str.toLowerCase();
     let left = 0;
-    let right = theLower.length -1;
+    let right = lowerStr.length-1
     while(left < right){
-        const l = theLower[left];
-        const r = theLower[right];
-        if(!l || !isAlphanumeric(l)){
+        if(!isAlphanumeric(lowerStr[right])){
+            right -= 1;
+            continue;
+        }
+        if(!isAlphanumeric(lowerStr[left])){
             left += 1;
             continue;
         }
-        if(!r || !isAlphanumeric(r)){
-            right -= 1
-            continue;
-        }
-        if(theLower [left] !== theLower [right]){
+        if(lowerStr[left] !== lowerStr[right]){
             return false;
         }
         left += 1;
         right -= 1;
-    }
-    
-    return true
-}
+    }    
+    return true;
+};
 
-function isAlphanumeric(c: string): boolean{
-    if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') ){
+function isAlphanumeric(c){
+    if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')){
         return true;
-
     }
-    return false
+    return false;
 }
 
 console.log(isPalindrome('A man, a plan, a canal: Panama'));
